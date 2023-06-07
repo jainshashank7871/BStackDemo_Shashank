311bb5ac91820bba4a69581259b97f9866521e96
jainshashank7871 <jainshashank7871@gmail.com>
2023-06-08 00:04:24 +0530

    Playwright with local setup

---
+++ playwright_local_setup/test.js
@@ -0,0 +1,13 @@
+const { chromium } = require('playwright');
+
+
+(async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto('https://www.example.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
