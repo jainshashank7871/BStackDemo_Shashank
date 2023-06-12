748a7c707afaaa8cd0c1be167820852e9b7b51b1
jainshashank7871 <jainshashank7871@gmail.com>
2023-06-12 17:11:18 +0530

    updated webdriver README

--- webdriverio-appium-app-browserstack/android/examples/run-sample-test/test.conf.js
+++ webdriverio-appium-app-browserstack/android/examples/run-sample-test/test.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
-  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+  user: process.env.BROWSERSTACK_USERNAME || 'shashank_IDHsQL',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'xmppCzYt4pUrhVRifaHs',
 
   hostname: 'hub.browserstack.com',
 
